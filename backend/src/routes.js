aef7a3f77bf9bbe9bb1694c276ec5d26a7fc88f6
João Paulo <musicjoaopaulo@gmail.com>
2020-04-21 23:41:17 -0300

    chore(backend): create the register of dev`s location

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,32 +1,9 @@
 const { Router } = require('express');
-const axios = require('axios');
 
-const Dev = require('./models/Dev');
+const DevController = require('./controllers/DevController');
 
 const routes = Router();
 
-routes.post('/devs', async (req, res) => {
-  const { github_username, techs } = req.body;
-
-  const response = await axios.get(
-    `https://api.github.com/users/${github_username}`
-  );
-
-  const { name = login, avatar_url, bio } = response.data;
-
-  const techsArr = techs.split(',').map((tech) => tech.trim());
-
-  // console.log(response.data);
-
-  const dev = await Dev.create({
-    github_username,
-    name,
-    avatar_url,
-    bio,
-    techs: techsArr,
-  });
-
-  return res.json(dev);
-});
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
