1701d596956ca695486145b170f1231291dc1490
João Paulo <musicjoaopaulo@gmail.com>
2020-04-21 22:50:21 -0300

    chore(backend): add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -12,7 +13,7 @@ mongoose.connect(process.env.MONGO_URL, {
   useUnifiedTopology: true,
 });
 
-// app.use(cors());
+app.use(cors());
 app.use(routes);
 app.use(express.json());
 
