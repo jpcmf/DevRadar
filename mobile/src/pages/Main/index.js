98b24604feb479d224fe65500f5e35b5bd2139bb
João Paulo <musicjoaopaulo@gmail.com>
2020-04-23 18:38:12 -0300

    chore(mobile): create profile webview

--- mobile/src/pages/Main/index.js
+++ mobile/src/pages/Main/index.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet } from 'react-native';
 
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import {
   requestPermissionsAsync,
   getCurrentPositionAsync,
 } from 'expo-location';
 
-// import { MapViewWrapper } from './styles';
+import { Avatar, Tooltip, DevName, DevBio, DevTechs } from './styles';
 
-function Main() {
+function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
 
   useEffect(() => {
@@ -42,12 +42,25 @@ function Main() {
   return (
     <MapView initialRegion={currentRegion} style={styles.map}>
       <Marker coordinate={{ latitude: -25.464894, longitude: -49.2861416 }}>
-        <Image
-          style={styles.avatar}
+        <Avatar
           source={{
             uri: 'https://avatars0.githubusercontent.com/u/1216136?v=4',
           }}
         />
+
+        <Callout
+          onPress={() => {
+            navigation.navigate('Profile', {
+              github_username: 'jpcmf',
+            });
+          }}
+        >
+          <Tooltip>
+            <DevName>Joao Paulo Fricks</DevName>
+            <DevBio>Bio...</DevBio>
+            <DevTechs>ReactJS, React Native</DevTechs>
+          </Tooltip>
+        </Callout>
       </Marker>
     </MapView>
   );
@@ -56,13 +69,6 @@ function Main() {
 const styles = StyleSheet.create({
   map: {
     flex: 1,
-  },
-  avatar: {
-    height: 54,
-    width: 54,
-    borderRadius: 4,
-    borderWidth: 4,
-    borderColor: '#fff',
   },
 });
 
