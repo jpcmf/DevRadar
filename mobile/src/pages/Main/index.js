07683c489310c12f4b6cb0e582b5782a2048f9f6
João Paulo <musicjoaopaulo@gmail.com>
2020-04-23 18:07:18 -0300

    chore(mobile): create mapview and mark

--- mobile/src/pages/Main/index.js
+++ mobile/src/pages/Main/index.js
@@ -1,8 +1,69 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Image } from 'react-native';
 
-// import { Container } from './styles';
+import MapView, { Marker } from 'react-native-maps';
+import {
+  requestPermissionsAsync,
+  getCurrentPositionAsync,
+} from 'expo-location';
 
-export default function Main() {
-  return <View />;
+// import { MapViewWrapper } from './styles';
+
+function Main() {
+  const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true,
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        });
+      }
+    }
+
+    loadInitialPosition();
+  }, []);
+
+  if (!currentRegion) {
+    return null;
+  }
+
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{ latitude: -25.464894, longitude: -49.2861416 }}>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri: 'https://avatars0.githubusercontent.com/u/1216136?v=4',
+          }}
+        />
+      </Marker>
+    </MapView>
+  );
 }
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+  avatar: {
+    height: 54,
+    width: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#fff',
+  },
+});
+
+export default Main;
